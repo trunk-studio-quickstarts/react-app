c072d030f0d9d0119334b3099cd7d1a67bbde5e6
smlsunxie <smlsun@gmail.com>
2018-10-18 14:50:28 +0800

    update

--- deploy-setup.sh
+++
@@ -1,3 +0,0 @@
-ORIGIN_REACT_APP_API_SERVER=http://localhost:5000/
-source .env.production
-find -name 'main.*.js' -exec sed -i -e "s|${ORIGIN_REACT_APP_API_SERVER}|${REACT_APP_API_SERVER}|g" '{}' \;
